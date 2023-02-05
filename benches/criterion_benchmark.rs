76ab11356464044826ec7a200e93d3d13a9f0dd0
Matt Willer <matt.r.willer@gmail.com>
2023-02-04 23:38:31 -0800

    Fix package name

--- benches/criterion_benchmark.rs
+++ benches/criterion_benchmark.rs
@@ -1,5 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use fhirpath::parser::Lexer;
+use maghemite::parser::Lexer;
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("Lexer/Patient.name.family.replace('er', 'iams')", |b| {
